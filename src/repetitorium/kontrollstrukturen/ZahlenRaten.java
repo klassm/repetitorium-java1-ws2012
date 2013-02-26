50cd553dfda8e712b6eccc37e2eb821d777466ff
Rico Lieback <rico@lieback.eu>
2013-02-26 15:11:15 +0100

    ZahlenRaten refactored

--- src/repetitorium/kontrollstrukturen/ZahlenRaten.java
+++ src/repetitorium/kontrollstrukturen/ZahlenRaten.java
@@ -6,15 +6,18 @@ public class ZahlenRaten {
 
     public static void main(String[] args) throws Exception{
 
-        int random = (new Random().nextInt(100));
-        int tries = 20;
+        final int tries = 20;
+        final int max = 100;
+        int random = (new Random().nextInt(max));
+
+        System.out.println("Hallo Spieler! Du hast " + tries + " Versuche! Die Zahl ist maximal " + max + "!");
 
         for(int i = 0; i < tries; i++) {
+            System.out.print("Versuch " + (i + 1) + ": ");
 
             int readNumber = 0;
             int currentChar = System.in.read();
-            while(currentChar != '\n' && currentChar != '\r')
-            {
+            while(currentChar != '\n' && currentChar != '\r') {
                 int integerValue = currentChar - '0';
                 readNumber = readNumber * 10 + integerValue;
                 currentChar = System.in.read();
@@ -31,11 +34,9 @@ public class ZahlenRaten {
                 System.out.println("Die eigegebene Zahl ist groesser als die gesuchte Zahl!");
 
             }
-
-
         }
 
-        System.out.println("Anzahl Versuche erschoepft. Ende.");
+        System.out.println("Das wars. Verloren :P");
     }
 
 
