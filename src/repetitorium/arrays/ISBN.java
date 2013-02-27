e7626cbbf1e7f4bc9e1748eec94abd3f7407481d
Rico Lieback <rico@lieback.eu>
2013-02-27 09:43:18 +0100

    ISBN implemented

--- src/repetitorium/arrays/ISBN.java
+++ src/repetitorium/arrays/ISBN.java
@@ -2,6 +2,14 @@ package repetitorium.arrays;
 
 public class ISBN {
     public static void main(String[] args) {
+        final int[] isbnNumber = {4,3,6,9,6,5,1,3,2};
 
+        int pruefNumber = 0;
+        for(int i = 0; i < isbnNumber.length; i++) {
+            pruefNumber += isbnNumber[i]*(i+1);
+        }
+        pruefNumber = pruefNumber % 11;
+
+        System.out.println("Pruefzahl: " + pruefNumber);
     }
 }
