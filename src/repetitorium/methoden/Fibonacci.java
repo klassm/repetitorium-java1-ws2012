472809bfb50f0b17f086bde383f807af2a7d6344
Rico Lieback <rico@lieback.eu>
2013-02-28 10:22:08 +0100

    formatting

--- src/repetitorium/methoden/Fibonacci.java
+++ src/repetitorium/methoden/Fibonacci.java
@@ -18,6 +18,7 @@ public class Fibonacci {
 
     public static void main(String[] args) {
         System.out.println(fibonacci(15));
+        System.out.println();
 
         System.out.println(0);
         System.out.println(1);
